56c8b0b6ac8cf4080fa80047c95860580fb0e2d7
Mert Yaman <mertyaman144@gmail.com>
2020-05-07 13:06:18 +0300

    change on message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 11130
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('"Hello Hepsiburada from Mert"!!!\n')
+  response.end('"Hello world"!!!\n')
 })
 
 server.listen(port)
